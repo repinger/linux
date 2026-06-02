3f0a0bbdd660e54ae98d2d926ae9d49ef8694188
Sultan Alsawaf <sultan@kerneltoast.com>
2026-06-02 11:21:45 +0700

    sched: Resolve sched_feat() at compile time to improve code optimization
    Keeping the compiler in the dark about which sched_feat() toggles are
    set reduces its ability to optimize scheduler code, which results in a
    measurably significant performance loss since the affected code is very
    hot. Turning sched_feat() definitions into macros that the compiler can
    resolve results in smaller and faster code.
    
    The sched_feat() definitions are converted to macros with this sed:
    sed -i -E \
    	-e 's/SCHED_FEAT\((.*),(.*)\)/#define SCHED_FEAT_\1\2/' \
    	-e 's/true/1/' \
    	-e 's/false/0/' \
    	kernel/sched/features.h
    
    The sched_feat() macros are named SCHED_FEAT_*. With this done, we can
    now optimize try-to-wake-up code when SCHED_FEAT_TTWU_QUEUE is disabled,
    which results in a CPU usage reduction of a few percent inside
    try_to_wake_up().
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -156,20 +156,6 @@ static int __init setup_proxy_exec(char *str)
 __setup("sched_proxy_exec", setup_proxy_exec);
 
 /*
- * Debugging: various feature bits
- *
- * If SCHED_DEBUG is disabled, each compilation unit has its own copy of
- * sysctl_sched_features, defined in sched.h, to allow constants propagation
- * at compile time and compiler optimization based on features default.
- */
-#define SCHED_FEAT(name, enabled)	\
-	(1UL << __SCHED_FEAT_##name) * enabled |
-__read_mostly unsigned int sysctl_sched_features =
-#include "features.h"
-	0;
-#undef SCHED_FEAT
-
-/*
  * Print a warning if need_resched is set for the given duration (if
  * LATENCY_WARN is enabled).
  *
@@ -3782,6 +3768,7 @@ static int ttwu_runnable(struct task_struct *p, int wake_flags)
 	return ret;
 }
 
+#if SCHED_FEAT_TTWU_QUEUE
 void sched_ttwu_pending(void *arg)
 {
 	struct llist_node *llist = arg;
@@ -3818,6 +3805,7 @@ void sched_ttwu_pending(void *arg)
 	WRITE_ONCE(rq->ttwu_pending, 0);
 	rq_unlock_irqrestore(rq, &rf);
 }
+#endif
 
 /*
  * Prepare the scene for sending an IPI for a remote smp_call
@@ -3835,6 +3823,7 @@ bool call_function_single_prep_ipi(int cpu)
 	return true;
 }
 
+#if SCHED_FEAT_TTWU_QUEUE
 /*
  * Queue a task on the target CPUs wake_list and wake the CPU via IPI if
  * necessary. The wakee CPU on receipt of the IPI will queue the task
@@ -3852,6 +3841,7 @@ static void __ttwu_queue_wakelist(struct task_struct *p, int cpu, int wake_flags
 	__smp_call_single_queue(cpu, &p->wake_entry.llist);
 #endif
 }
+#endif
 
 void wake_up_if_idle(int cpu)
 {
@@ -3896,6 +3886,7 @@ bool cpus_share_resources(int this_cpu, int that_cpu)
 	return per_cpu(sd_share_id, this_cpu) == per_cpu(sd_share_id, that_cpu);
 }
 
+#if SCHED_FEAT_TTWU_QUEUE
 static inline bool ttwu_queue_cond(struct task_struct *p, int cpu)
 {
 	/* See SCX_OPS_ALLOW_QUEUED_WAKEUP. */
@@ -3955,14 +3946,17 @@ static bool ttwu_queue_wakelist(struct task_struct *p, int cpu, int wake_flags)
 
 	return false;
 }
+#endif
 
 static void ttwu_queue(struct task_struct *p, int cpu, int wake_flags)
 {
 	struct rq *rq = cpu_rq(cpu);
 	struct rq_flags rf;
 
+#if SCHED_FEAT_TTWU_QUEUE
 	if (ttwu_queue_wakelist(p, cpu, wake_flags))
 		return;
+#endif
 
 	rq_lock(rq, &rf);
 	update_rq_clock(rq);
@@ -4244,6 +4238,7 @@ int try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 		 */
 		WRITE_ONCE(p->__state, TASK_WAKING);
 
+#if SCHED_FEAT_TTWU_QUEUE
 		/*
 		 * If the owning (remote) CPU is still in the middle of schedule() with
 		 * this task as prev, considering queueing p on the remote CPUs wake_list
@@ -4266,6 +4261,7 @@ int try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 		if (smp_load_acquire(&p->on_cpu) &&
 		    ttwu_queue_wakelist(p, task_cpu(p), wake_flags))
 			break;
+#endif
 
 		/*
 		 * If the owning (remote) CPU is still in the middle of schedule() with
